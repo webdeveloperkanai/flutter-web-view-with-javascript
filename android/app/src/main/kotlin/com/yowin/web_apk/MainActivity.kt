d23ee113045f53a29bf0506ac0163e56da1a23a8
webdeveloperkanai <kanaishil501@gmail.com>
2023-06-04 10:04:40 +0530

    Trying to add onesignal

--- android/app/src/main/kotlin/com/yowin/web_apk/MainActivity.kt
+++ android/app/src/main/kotlin/com/yowin/web_apk/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.devil.win
+import com.onesignal.OneSignal
+const val ONESIGNAL_APP_ID = "50eefdeb-38d7-43be-a5ba-86237caeb417"
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+      
+      // OneSignal Initialization
+      OneSignal.initWithContext(this)
+      OneSignal.setAppId(ONESIGNAL_APP_ID)
+        
+      // promptForPushNotifications will show the native Android notification permission prompt.
+      // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+      OneSignal.promptForPushNotifications();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 const APP_API = "https://color.devsecit.com/api.php";
 const APP_UPDATE = "https://color.devsecit.com/COLOR.apk";
@@ -16,8 +17,157 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _enableConsentButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    if (!mounted) return;
+
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+    OneSignal.shared.setRequiresUserPrivacyConsent(false);
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      print('NOTIFICATION OPENED HANDLER CALLED WITH: ${result}');
+      setState(() {});
+    });
+
+    OneSignal.shared.setNotificationWillShowInForegroundHandler(
+        (OSNotificationReceivedEvent event) {
+      print('FOREGROUND HANDLER CALLED WITH: ${event}');
+
+      /// Display Notification, send null to not display
+      event.complete(null);
+    });
+
+    OneSignal.shared
+        .setInAppMessageClickedHandler((OSInAppMessageAction action) {});
+
+    OneSignal.shared
+        .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+      print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+      print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setEmailSubscriptionObserver(
+        (OSEmailSubscriptionStateChanges changes) {
+      print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared
+        .setSMSSubscriptionObserver((OSSMSSubscriptionStateChanges changes) {
+      print("SMS SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setOnWillDisplayInAppMessageHandler((message) {
+      print("ON WILL DISPLAY IN APP MESSAGE ${message.messageId}");
+    });
+
+    OneSignal.shared.setOnDidDisplayInAppMessageHandler((message) {
+      print("ON DID DISPLAY IN APP MESSAGE ${message.messageId}");
+    });
+
+    OneSignal.shared.setOnWillDismissInAppMessageHandler((message) {
+      print("ON WILL DISMISS IN APP MESSAGE ${message.messageId}");
+    });
+
+    OneSignal.shared.setOnDidDismissInAppMessageHandler((message) {
+      print("ON DID DISMISS IN APP MESSAGE ${message.messageId}");
+    });
+
+    // NOTE: Replace with your own app ID from https://www.onesignal.com
+    await OneSignal.shared.setAppId("50eefdeb-38d7-43be-a5ba-86237caeb417");
+
+    // iOS-only method to open launch URLs in Safari when set to false
+    OneSignal.shared.setLaunchURLsInApp(false);
+
+    bool requiresConsent = await OneSignal.shared.requiresUserPrivacyConsent();
+
+    setState(() {
+      _enableConsentButton = requiresConsent;
+    });
+
+    // Some examples of how to use In App Messaging public methods with OneSignal SDK
+    oneSignalInAppMessagingTriggerExamples();
+
+    OneSignal.shared.disablePush(false);
+
+    // Some examples of how to use Outcome Events public methods with OneSignal SDK
+    oneSignalOutcomeEventsExamples();
+
+    bool userProvidedPrivacyConsent =
+        await OneSignal.shared.userProvidedPrivacyConsent();
+    print("USER PROVIDED PRIVACY CONSENT: $userProvidedPrivacyConsent");
+  }
+
+  oneSignalInAppMessagingTriggerExamples() async {
+    OneSignal.shared.addTrigger("trigger_1", "one");
+
+    Map<String, Object> triggers = new Map<String, Object>();
+    triggers["trigger_2"] = "two";
+    triggers["trigger_3"] = "three";
+    OneSignal.shared.addTriggers(triggers);
+
+    OneSignal.shared.removeTriggerForKey("trigger_2");
+
+    // Get the value for a trigger by its key
+    Object? triggerValue =
+        await OneSignal.shared.getTriggerValueForKey("trigger_3");
+    print("'trigger_3' key trigger value: ${triggerValue?.toString()}");
+
+    // Create a list and bulk remove triggers based on keys supplied
+    List<String> keys = ["trigger_1", "trigger_3"];
+    OneSignal.shared.removeTriggersForKeys(keys);
+
+    // Toggle pausing (displaying or not) of IAMs
+    OneSignal.shared.pauseInAppMessages(false);
+  }
+
+  oneSignalOutcomeEventsExamples() async {
+    // Await example for sending outcomes
+    outcomeAwaitExample();
+
+    // Send a normal outcome and get a reply with the name of the outcome
+    OneSignal.shared.sendOutcome("normal_1");
+    OneSignal.shared.sendOutcome("normal_2").then((outcomeEvent) {
+      print(outcomeEvent.jsonRepresentation());
+    });
+
+    // Send a unique outcome and get a reply with the name of the outcome
+    OneSignal.shared.sendUniqueOutcome("unique_1");
+    OneSignal.shared.sendUniqueOutcome("unique_2").then((outcomeEvent) {
+      print(outcomeEvent.jsonRepresentation());
+    });
+
+    // Send an outcome with a value and get a reply with the name of the outcome
+    OneSignal.shared.sendOutcomeWithValue("value_1", 3.2);
+    OneSignal.shared.sendOutcomeWithValue("value_2", 3.9).then((outcomeEvent) {
+      print(outcomeEvent.jsonRepresentation());
+    });
+  }
+
+  Future<void> outcomeAwaitExample() async {
+    var outcomeEvent = await OneSignal.shared.sendOutcome("await_normal_1");
+    print(outcomeEvent.jsonRepresentation());
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
